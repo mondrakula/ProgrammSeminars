8d92e3868f2a670104363564ffe72b8c96b150ec
Vladislav <mondrakula@mail.ru>
2022-12-21 22:33:03 +0300

    test 1

---
+++ семинар 1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number2 * number2;
+if(number1 == result)
+    Console.WriteLine($"Квадрат = {result}");
+else 
+    Console.WriteLine("Первое число НЕ равняется квадратом второго");
