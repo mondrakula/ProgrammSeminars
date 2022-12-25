8403a05b255bdef55d5c224c0b4274fbcb005ccb
Vladislav <mondrakula@mail.ru>
2022-12-25 23:34:45 +0300

    Задача10

--- семинар 2/Задача10/Program.cs
+++ семинар 2/Задача10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string digit = Convert.ToString(number);
+Console.WriteLine($"Вторая цифра числа {number} = {digit[1]}");
