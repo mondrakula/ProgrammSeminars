b82ff7c73c3bdf897b5d9c02c454fc4e8bdf6f67
Vladislav <mondrakula@mail.ru>
2022-12-25 23:21:56 +0300

    Функции с возратом переменной и без

---
+++ семинар 1/ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int index = 0;
+    int length = collection.Length;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int ind = 0;
+    int leng = col.Length;
+    while (ind < leng)
+    {
+        Console.Write($"{col[ind]} ");
+        ind++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int length = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < length)
+    {
+        if (collection[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
