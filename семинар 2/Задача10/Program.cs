fa91ae1e9c72bf8b634510dacf10e7a82beb429e
Vladislav <mondrakula@mail.ru>
2022-12-25 23:32:08 +0300

    Функции с возратом переменной и бе

--- семинар 2/Задача10/Program.cs
+++ семинар 2/Задача10/Program.cs
@@ -1,13 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int number = new Random().Next(1,10);
-Console.WriteLine(number);
-string username = Console.ReadLine();
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine("Привет, Маша!");
-}
-else
-{
-    Console.WriteLine("Привет");
-}
+Console.Clear();
