6a78cae871627562b2259784374ffcc3eb7871cb
Vladislav <mondrakula@mail.ru>
2023-02-06 00:20:07 +0300

    Двумерные массивы. Продолжение

--- смеинар 8/Задача62/Program.cs
+++ смеинар 8/Задача62/Program.cs
@@ -1,4 +1,37 @@
 ﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 int[,] spiralMatrix = new int[4,4];
 
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
+{
+  spiralMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 & i + j < spiralMatrix.GetLength(0) - 1)
+    j++;
+  else if (i < j & i + j >= spiralMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j & i + j > spiralMatrix.GetLength(0) - 1)
+    j--;
+  else
+    i--;
+}
+PrintMatrix(spiralMatrix);
